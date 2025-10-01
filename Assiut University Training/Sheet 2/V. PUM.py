0c39c7eec347854e355b16665b22854b96e5bcb8
Mohammed Atef <106938379+MohammedAtef0@users.noreply.github.com>
2025-10-01 13:05:56 +0300

    Create V. PUM.py

---
+++ Assiut University Training/Sheet 2/V. PUM.py
@@ -0,0 +1,5 @@
+N = int(input())
+for i in range(1, N + 1):
+    # start = 4 * (i - 1) + 1
+    # print 3 numbers and "PUM"
+    print(4 * (i - 1) + 1, 4 * (i - 1) + 1 + 1, 4 * (i - 1) + 1 + 2, "PUM")
